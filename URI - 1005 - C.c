0002f53f28398a22c772679bbef9d9d8c025e9b1
Hugo Esteves <hugo.esteves@unesp.br>
2020-10-12 22:33:32 -0300

    Exercícios URI

---
+++ URI - 1005 - C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	double x, y, MEDIA;
+	scanf ("%lf", &x);
+	scanf ("%lf", &y);
+	MEDIA = ((x*3.5)+(y*7.5))/11;
+	printf ("MEDIA = %.5lf\n", MEDIA);
+	return 0;
+
+}
