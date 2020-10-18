8b35b57730a1b5b0cdf18b5ede7e08a1bc53dbd4
Hugo Esteves <hugo.esteves@unesp.br>
2020-10-18 20:12:02 -0300

    Create URI - 1010 - C.c

---
+++ URI - 1010 - C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+int A,B,A2,B2;
+double C,C2,total;
+scanf("%d %d %lf", &A,&B,&C) ==1;
+scanf("%d %d %lf",  &A2,&B2,&C2) ==1;
+total = (B*C)+(B2*C2);
+printf("VALOR A PAGAR: R$ %.2lf\n", total);
+
+}
